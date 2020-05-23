16256b29b6906af5264a9590cbb0e58f37df55fd
Dima Tronciu <tronciud@gmail.com>
2020-05-23 21:51:02 +0300

    Prepared project for starting

---
+++ src/screens/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+function App() {
+  return (
+      <View>
+        <Text>Test</Text>
+      </View>
+  )
+}
+
+export default App;
