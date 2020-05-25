321b22175b194ae69bc1ad35ca8698e33f18441f
Dima Tronciu <tronciud@gmail.com>
2020-05-25 16:46:44 +0300

    Created redux store and stored post content

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, RefreshControl} from 'react-native';
+import {createStore} from "redux";
 import styled from 'styled-components/native';
 import Button from '../components/Button';
 import {cardContentUrl} from '../constants';
@@ -9,15 +10,24 @@ function PostScreen() {
   const [cardList, setCardList] = useState([]);
   const networkErrorMessage = 'Please check out your network!';
 
-  async function fetchCardContentData() {
+  const store = createStore((state=[], action) => {
+    return action.type === 'ADD_CARD' ? state.concat([action.text]) : state
+  })
+
+  function fetchCardContentData() {
     fetch(cardContentUrl)
       .then(res => res.json())
       .then(data => setCardList(data))
       .catch(err => alert(`${err}.\n${networkErrorMessage}`));
   }
 
+  function storeCards(body) {
+    store.dispatch({ type: 'ADD_CARD', body }, ['Post card list'])
+  }
+
   useEffect(() => {
     fetchCardContentData();
+    storeCards(cardList)
   }, []);
 
   return (
