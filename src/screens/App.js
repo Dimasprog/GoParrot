24497feeb91afda5db08dd0badda423ffe6b1bfc
Dima Tronciu <tronciud@gmail.com>
2020-06-01 02:11:13 +0300

    Updated gif for 2nd phase!

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import PostScreen from "./PostScreen";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
 import Status from "../components/Status";
-
-const store = createStore((posts=[], action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS':
-      return action.postList
-    case 'PIN_POST':
-      return posts.map(post => post.id === action.postId ? {...post, isPinned: true} : post)
-    case 'UNPIN_POST':
-      return posts.map(post => post.id === action.postId ? {...post, isPinned: false} : post)
-    default:
-      return posts
-  }
-})
+import {store} from "../components/Store";
 
 const Stack = createStackNavigator();
 
